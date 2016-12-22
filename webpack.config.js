84a2b3b21624ee314c51d05275b07d236d960bad
Marc Garreau <marcdgarreau@gmail.com>
2016-12-22 15:04:13 -0700

    Tweaks babel settings

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: '/node_modules/',
-        loader: 'babel-loader',
-        query: { presets: ['es2015', 'react'] }
+        loader: 'babel-loader'
       }
     ]
   },
